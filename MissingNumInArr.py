bef5827d44db7e2bd452ba58b037416d0edc656d
kimberlybone <k_bone@uncg.edu>
2020-11-29 17:30:59 -0500

    updates to missingNumber

--- MissingNumInArr.py
+++ MissingNumInArr.py
@@ -1,4 +1,4 @@
 # Task:
 # Complete the function MissingNumber() that takes array and N as input and returns the value of the missing number.
-
+# This method uses the summation formula
 def MissingNumber(array,n):
