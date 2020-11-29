e074ce6332c0b065132b24b6b5418104bb40172b
kimberlybone <k_bone@uncg.edu>
2020-11-29 17:50:26 -0500

    solution to missingNumber

--- MissingNumInArr.py
+++ MissingNumInArr.py
@@ -2,3 +2,9 @@
 # Complete the function MissingNumber() that takes array and N as input and returns the value of the missing number.
 # This method uses the summation formula
 def MissingNumber(array,n):
+    sumTotal = n*(n+1)/2
+    total = 0
+    for i in range(len(array)):
+        total += array[i]
+    missingNum = sumTotal - total
+    print(missingNum)
