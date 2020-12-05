cd924710d60e1c34b4d4b48fc6db2a5089a38bb6
kimberlybone <k_bone@uncg.edu>
2020-12-05 15:54:35 -0500

    missingNumInArr completed and MajorityElement file started

--- GeeksForGeeks/MissingNumInArr.py
+++ GeeksForGeeks/MissingNumInArr.py
@@ -1,10 +1,11 @@
 # Task:
 # Complete the function MissingNumber() that takes array and N as input and returns the value of the missing number.
 # This method uses the summation formula
+# The len of arr is n-1
 def MissingNumber(array,n):
     sumTotal = n*(n+1)/2
     total = 0
     for i in range(len(array)):
         total += array[i]
-    missingNum = sumTotal - total
+    missingNum = int(sumTotal - total)
     print(missingNum)
