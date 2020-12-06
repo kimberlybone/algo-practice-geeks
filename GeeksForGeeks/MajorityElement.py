e4b686618062403c9e0bcc07e70506027ddcc627
kimberlybone <k_bone@uncg.edu>
2020-12-06 17:33:36 -0500

    updates to MajorityElement

--- GeeksForGeeks/MajorityElement.py
+++ GeeksForGeeks/MajorityElement.py
@@ -3,8 +3,15 @@
 # A majority element is an element that appears more than N/2 times in the array
 # Return "-1" if no majority exists
 def majorityElement(A,N):
+    A.sort()
+    prevElem = 0
+    majCount = N/2
+    count = 0
     for i in range(len(A)):
-        if A[i] > N/2:
-            return A[i]
+        prevElem = A[i - 1]
+        if A[i] == prevElem:
+            ++count
         else:
-            return -1
+            count
+        if count > N/2:
+            return A[i]
